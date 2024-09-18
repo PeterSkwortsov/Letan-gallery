831db10ccf546793f928d596ae9130d70502b3e2
Peter <skwortsow.petia@yandex.ru>
2024-09-18 11:49:20 +0300

    Добавил lazy loader на страницы

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
+import { lazy, Suspense } from "react";
 import "./App.css";
-import Kolosov from "./components/Kolosov";
-import Skvortsova from "./components/Skvortsova";
+// import Kolosov from "./components/Kolosov";
+// import Skvortsova from "./components/Skvortsova";
 import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
-import Footer from "./components/Footer";
-import Biography from "./components/Bio/Biography";
-import WinterCollect from "./components/WinterCollect";
-import Flowers from "./components/Flowers";
-import Animals from "./components/Animals";
-import Blog from "./components/Blog";
+// import Footer from "./components/Footer";
+// import Biography from "./components/Bio/Biography";
+// import WinterCollect from "./components/WinterCollect";
+// import Flowers from "./components/Flowers";
+// import Animals from "./components/Animals";
+// import Blog from "./components/Blog";
 import { useState } from "react";
-import TownHistory from "./components/Town-history";
+// import TownHistory from "./components/Town-history";
+import GridLoader from "react-spinners/GridLoader";
+
+const Biography = lazy(() => import("./components/Bio/Biography"));
+const Kolosov = lazy(() => import("./components/Kolosov"));
+const WinterCollect = lazy(() => import("./components/WinterCollect"));
+const TownHistory = lazy(() => import("./components/Town-history"));
+// const MainLayout = lazy(() => import("./layouts/MainLayout"));
+const Footer = lazy(() => import("./components/Footer"));
+const Blog = lazy(() => import("./components/Blog"));
+const Animals = lazy(() => import("./components/Animals"));
+const Flowers = lazy(() => import("./components/Flowers"));
+const Skvortsova = lazy(() => import("./components/Skvortsova"));
+
 
 function App() {
   const [cart, setCart] = useState([]);
@@ -23,7 +37,6 @@ function App() {
   const removeFromCart = (item) => {
     setCart(cart.filter((i) => i.name !== item.name));
   };
- 
 
   return (
     <>
@@ -33,10 +46,44 @@ function App() {
             <Route
               path="/"
               element={
-                <MainLayout cart={cart} removeFromCart={removeFromCart} />
+                <Suspense
+                  fallback={
+                    <GridLoader
+                      color="#79d9cc"
+                      style={{
+                        margin: "0 auto",
+                        display: "flex",
+                        padding: "30px",
+                      }}
+                      size={50}
+                    />
+                  }
+                >
+                  <MainLayout cart={cart} removeFromCart={removeFromCart} />
+                </Suspense>
               }
             >
-              <Route path="/" element={<Biography />} />
+              <Route
+                path="/"
+                element={
+                  <Suspense
+                    fallback={
+                      <GridLoader
+                        color="#79d9cc"
+                        style={{
+                          margin: "0 auto",
+                          display: "flex",
+                          padding: "20px",
+                        }}
+                        size={50}
+                      />
+                    }
+                  >
+                    <Biography />
+                  </Suspense>
+                }
+              />
+
               <Route
                 path="kolosov"
                 element={<Kolosov cart={cart} addToCart={addToCart} />}
